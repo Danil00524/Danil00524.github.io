37e0a625a2302d196dcefa2136083dcf721d7fff
Darvin00524 <jerry.lewis.green@gmail.com>
2019-08-23 16:11:23 +0300

    all new project

---
+++ Test-work-3/gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp'),
+  scss = require('gulp-sass'),
+  browserSync = require('browser-sync'),
+  uglifyjs = require('gulp-uglifyjs'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename'),
+  autoprefixer = require('gulp-autoprefixer');
+  pug = require('gulp-pug');
+
+gulp.task('pug', function(){
+  return gulp.src('app/*.pug')
+    .pipe(pug(
+      // {pretty: true}
+    ))
+    .pipe(gulp.dest('app'))
+})
+
+gulp.task('scss', function () {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 8 versions'],
+    }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('script', function () {
+  return gulp.src('app/js/**/*.js')
+    .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('code', function () {
+  return gulp.src('app/*.html')
+    .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  })
+});
+
+gulp.task('js', function () {
+  return gulp.src(
+    ['node_modules/jquery-form-styler/dist/jquery.formstyler.js'])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglifyjs())
+    .pipe(gulp.dest('app/js'))
+});
+
+gulp.task('watch', function () {
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
+  gulp.watch('app/js/**/*.js', gulp.parallel('script'))
+  gulp.watch('app/*.pug', gulp.parallel('pug'))
+});
+
+gulp.task('default', gulp.parallel('scss', 'browser-sync', 'js', 'watch'));
